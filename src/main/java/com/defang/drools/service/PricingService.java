a7ea18e2b1b3b4d51f710620c4e66760315dd87b
shiming <codingmylife@gmail.com>
2019-07-08 11:23:35 +0800

    doc: update comments

--- src/main/java/com/defang/drools/service/PricingService.java
+++ src/main/java/com/defang/drools/service/PricingService.java
@@ -7,5 +7,11 @@ import com.defang.drools.model.Result;
  * pricing service
  */
 public interface PricingService {
+    /**
+     * based on the order(customer type, total price etc) to determine the discount, reduction and final price
+     *
+     * @param order
+     * @return result
+     */
     Result getTheResult(Order order);
 }
